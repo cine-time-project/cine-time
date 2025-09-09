1f08fa741c2bd682155852ada808799e3afbcf26
Ertugrulk25 <ek122302@gmail.com>
2025-09-09 13:27:09 +0300

    First commit, created project

---
+++ src/main/java/com/cinetime/CineTimeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cinetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineTimeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineTimeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinetime/cinetime/project/CinetimeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinetime.cinetime.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinetimeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
