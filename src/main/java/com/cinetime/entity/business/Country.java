e7025ae918dd1057582bcc7c909ffe0f23793734
Yasar TAY <yasartay@gmail.com>
2025-09-10 23:00:07 +0200

    feat: Country Entity is created

---
+++ src/main/java/com/cinetime/entity/business/Country.java
@@ -0,0 +1,27 @@
+package com.cinetime.entity.business;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Country {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  // TODO: 30 karakter sınırlaması bu şekilde yalnız DB'de mi yapılmalı
+  //  yoksa RequestDTO'sunda @Size ile de mi yapılmalı
+  @Column(nullable = false, length = 30)
+  private String name;
+}
