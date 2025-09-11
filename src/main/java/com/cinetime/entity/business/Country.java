53a7ac3cb08c895a3ad12d47b0514759595ecbf4
Yasar TAY <yasartay@gmail.com>
2025-09-11 22:50:11 +0200

    feature: Country Entity'si oluşturuldu.

---
+++ src/main/java/com/cinetime/entity/business/Country.java
@@ -0,0 +1,29 @@
+package com.cinetime.entity.business;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Country {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, length = 30, unique = true)
+  private String name;
+
+  //TODO: City Entity'si ile bidirectional relation kurulacak.
+
+}
