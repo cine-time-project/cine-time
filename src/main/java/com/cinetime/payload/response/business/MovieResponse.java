cd4d92e7d959f9c4f988e58b1f534fb88ed2ab6b
ahsenpaksoy <ahsenpaksoy@gmail.com>
2025-09-16 20:13:52 +0200

    MovieResponse class added

---
+++ src/main/java/com/cinetime/payload/response/business/MovieResponse.java
@@ -0,0 +1,37 @@
+package com.cinetime.payload.response.business;
+
+import com.cinetime.entity.business.Image;
+import com.cinetime.entity.enums.MovieStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MovieResponse {
+
+    private Long id;
+    private String title;
+    private String slug;
+    private String summary;
+    private LocalDate releaseDate;
+    private Integer duration;
+    private Double rating;
+    private String director;
+    private List<String> cast;
+    private List<String> formats;
+    private List<String> genre;
+    private MovieStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Image poster;
+
+
+}
