0704a5f2b62d1bec78e9300dd9b221811cdfe9ab
Yasar TAY <yasartay@gmail.com>
2025-09-16 23:28:46 +0200

    Feature: Controller-Service-Repository classes created for User. Relevant .gitkeep files are deleted

---
+++ src/main/java/com/cinetime/controller/user/UserController.java
@@ -0,0 +1,15 @@
+package com.cinetime.controller.user;
+
+import com.cinetime.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+  private UserService userService;
+
+}
