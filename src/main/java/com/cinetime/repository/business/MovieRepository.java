1388ee66a07dd5247a7e973a64a5b6d52cf660a6
ahsenpaksoy <ahsenpaksoy@gmail.com>
2025-09-18 00:47:24 +0200

    added query findAllBySlugIgnoreCase to join with Cinema entity

--- src/main/java/com/cinetime/repository/business/MovieRepository.java
+++ src/main/java/com/cinetime/repository/business/MovieRepository.java
@@ -4,8 +4,12 @@ import com.cinetime.entity.business.Movie;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MovieRepository extends JpaRepository<Movie,Long> {
 
     Page<Movie> findByTitleContainingIgnoreCaseOrSummaryContainingIgnoreCase(String title, String summary, Pageable pageable);
+
+    Page<Movie> findAllBySlugIgnoreCase(String cinemaSlug, Pageable pageable);
 }
