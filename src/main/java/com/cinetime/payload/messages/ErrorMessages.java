836618423b3914f296167fb0866a24bace4709a3
levenmartinov <levkomartinov@gmail.com>
2025-09-18 22:51:00 +0100

    Added Image Error and Success Messages

--- src/main/java/com/cinetime/payload/messages/ErrorMessages.java
+++ src/main/java/com/cinetime/payload/messages/ErrorMessages.java
@@ -12,5 +12,19 @@ public class ErrorMessages {
   public static final String MOVIE_NOT_FOUND = "Movie is not found";
   public static final String MOVIE_NOT_FOUND_BY_ID = "Movie is not found by id: %s";
 
+  //image
+  // 404 - Not Found
+  public static final String IMAGE_NOT_FOUND_ID = "Image not found with id: %d";
+  public static final String MOVIE_NOT_FOUND_ID = "Movie not found with id: %d";
+
+  // 400 - Bad Request
+  public static final String FILE_MUST_NOT_BE_EMPTY   = "file must not be null or empty";
+  public static final String FILE_SIZE_EXCEEDED       = "file size must be <= 5MB";
+  public static final String UNSUPPORTED_CONTENT_TYPE = "unsupported content type: %s";
+
+  // 409 - Conflict
+  public static final String MOVIE_ALREADY_HAS_POSTER           = "This movie already has a poster.";
+  public static final String MOVIE_ALREADY_HAS_DIFFERENT_POSTER = "This movie already has a different poster.";
+
 
 }
