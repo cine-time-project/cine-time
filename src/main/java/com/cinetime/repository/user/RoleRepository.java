4009158583dec23a921d2b4dd5efc302afbc5ca3
Esra Akineden <e.akineden@gmail.com>
2025-09-19 08:59:50 +0200

    Create admin if there is no user

--- src/main/java/com/cinetime/repository/user/RoleRepository.java
+++ src/main/java/com/cinetime/repository/user/RoleRepository.java
@@ -5,6 +5,9 @@ import com.cinetime.entity.business.Role;
 import com.cinetime.entity.enums.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
     boolean existsByRoleName(RoleName roleName);
+    Optional<Role> findByRoleName(RoleName roleName);
 }
