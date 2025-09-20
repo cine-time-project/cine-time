b80421c8ddcc5129179a1deb31853359a23a0fbc
levenmartinov <levkomartinov@gmail.com>
2025-09-20 16:38:58 +0100

    Added "/api/images", /api/images/**" in the SecurityConfig class

--- src/main/java/com/cinetime/security/config/SecurityConfig.java
+++ src/main/java/com/cinetime/security/config/SecurityConfig.java
@@ -26,79 +26,81 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-  private final UserDetailsServiceImpl userDetailsService;
-  private final AuthEntryPointJwt authEntryPointJwt;
-  private final JwtService jwtService;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthEntryPointJwt authEntryPointJwt;
+    private final JwtService jwtService;
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf(csrf -> csrf.disable())
-        .exceptionHandling(ex ->
-            ex.authenticationEntryPoint(authEntryPointJwt)
-        )
-        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers(AUTH_WHITELIST).permitAll()
-            .anyRequest().authenticated()
-        );
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .exceptionHandling(ex ->
+                        ex.authenticationEntryPoint(authEntryPointJwt)
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(AUTH_WHITELIST).permitAll()
+                        .anyRequest().authenticated()
+                );
 
-    http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
-    return http.build();
-  }
+        return http.build();
+    }
 
-  @Bean
-  public JwtAuthenticationFilter jwtAuthenticationFilter() {
-    return new JwtAuthenticationFilter(jwtService, userDetailsService);
-  }
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtService, userDetailsService);
+    }
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-  @Bean
-  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
-      throws Exception {
-    return configuration.getAuthenticationManager();
-  }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+            throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            //we let all sources to call our APIs
-            .allowedOrigins("*")
-            .allowedHeaders("*")
-            .allowedMethods("*");
-      }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        //we let all sources to call our APIs
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+
+    private static final String[] AUTH_WHITELIST = {
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/api-docs",
+            "/api-docs/**",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/webjars/**",
+            "/",
+            "/index.html",
+            "/images/**",
+            "/api/images",
+            "/api/images/**",
+            "/css/**",
+            "/js/**",
+            "/auth/login",
+            "/api/cinemas",
+            "/api/cinemas/**",
+            "/api/movies",
+            "/api/movies/**",
+            "/error"
     };
-  }
-
-  private static final String[] AUTH_WHITELIST = {
-      "/v3/api-docs",
-      "/v3/api-docs/**",
-      "/api-docs",
-      "/api-docs/**",
-      "/swagger-ui.html",
-      "/swagger-ui/**",
-      "/swagger-resources",
-      "/swagger-resources/**",
-      "/webjars/**",
-      "/",
-      "/index.html",
-      "/images/**",
-      "/css/**",
-      "/js/**",
-      "/auth/login",
-      "/api/cinemas",
-      "/api/cinemas/**",
-      "/api/movies",
-      "/api/movies/**",
-      "/error"
-  };
 
 }
