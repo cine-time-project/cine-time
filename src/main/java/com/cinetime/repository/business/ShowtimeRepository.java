f8a3bfb2f6429c9fba26c20eea5afc1a15dad288
ahsenpaksoy <ahsenpaksoy@gmail.com>
2025-09-20 22:36:17 +0200

    add queries for movies in theatres

--- src/main/java/com/cinetime/repository/business/ShowtimeRepository.java
+++ src/main/java/com/cinetime/repository/business/ShowtimeRepository.java
@@ -1,9 +1,13 @@
 package com.cinetime.repository.business;
 
 import com.cinetime.entity.business.Showtime;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public interface ShowtimeRepository extends JpaRepository<Showtime, Long> {
@@ -25,4 +29,9 @@ public interface ShowtimeRepository extends JpaRepository<Showtime, Long> {
         order by h.name asc, m.title asc, s.date asc, s.startTime asc
     """)
     List<HallMovieTimeRow> findShowtimesByCinemaId(@Param("cinemaId") Long cinemaId);
+
+    Page<Showtime> findByDate(LocalDate date, Pageable pageable);
+
+    Page<Showtime> findByDateAndStartTimeAfter(LocalDate date, LocalTime time, Pageable pageable);
+
 }
