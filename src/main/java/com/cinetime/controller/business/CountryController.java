83a909f5d7e14202374bc12e3b9fe4edadd6215d
MustafaCaldiran <126662774+MustafaCaldiran@users.noreply.github.com>
2025-10-30 02:40:47 -0400

    country, city, district dashboard page update
    country, city, district dashboard page update

--- src/main/java/com/cinetime/controller/business/CountryController.java
+++ src/main/java/com/cinetime/controller/business/CountryController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,16 +35,16 @@ public class CountryController {
     }
 
     @PreAuthorize("hasAuthority('ADMIN')")
-    @PutMapping("/update")
+    @PutMapping("/update/{countryId}")
     public ResponseEntity<ResponseMessage> updateCountry(
-        @RequestParam Long countryId, @RequestBody @Valid CountryRequest countryRequest){
+        @PathVariable Long countryId, @RequestBody @Valid CountryRequest countryRequest){
         return countryService.updateCountry(countryId,countryRequest);
     }
-
+@Transactional
     @PreAuthorize("hasAuthority('ADMIN')")
-    @DeleteMapping("/delete")
+    @DeleteMapping("/delete/{countryId}")
     public ResponseEntity<ResponseMessage> deleteCountry(
-            @RequestParam Long countryId){
+            @PathVariable Long countryId){
         return countryService.deleteCountry(countryId);
     }
 
--- src/main/java/com/cinetime/payload/mappers/CityMapper.java
+++ src/main/java/com/cinetime/payload/mappers/CityMapper.java
@@ -3,6 +3,7 @@ package com.cinetime.payload.mappers;
 import com.cinetime.entity.business.City;
 import com.cinetime.payload.request.business.CityRequest;
 import com.cinetime.payload.response.business.CityMiniResponse;
+import com.cinetime.payload.response.business.CountryMiniResponse;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.stereotype.Component;
@@ -12,9 +13,16 @@ import org.springframework.stereotype.Component;
 public class CityMapper {
     public CityMiniResponse cityToCityMiniResponse(City city){
         if (city==null) return null;
+
         return CityMiniResponse.builder()
                 .name(city.getName())
                 .id(city.getId())
+                .countryMiniResponse(
+                               CountryMiniResponse.builder()
+                                       .id(city.getCountry().getId())
+                                       .name(city.getCountry().getName())
+                                       .build()
+                )
                 .build();
     }
 
